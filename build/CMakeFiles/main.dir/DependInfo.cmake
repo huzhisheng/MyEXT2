342c6bfcff2de276cea2bf2fef12420527e7f86a
huzhisheng <1216265845@qq.com>
2020-12-11 23:59:14 +0800

    基本功能实现完毕 接下来还需实现的功能: 1.cd 2.动态分配磁盘块 3.读写文件

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/huhu/HITSZ2020_OS/MyEXT2/src/disk.c" "/home/huhu/HITSZ2020_OS/MyEXT2/build/CMakeFiles/main.dir/src/disk.c.o"
+  "/home/huhu/HITSZ2020_OS/MyEXT2/src/fs.c" "/home/huhu/HITSZ2020_OS/MyEXT2/build/CMakeFiles/main.dir/src/fs.c.o"
+  "/home/huhu/HITSZ2020_OS/MyEXT2/src/main.c" "/home/huhu/HITSZ2020_OS/MyEXT2/build/CMakeFiles/main.dir/src/main.c.o"
+  "/home/huhu/HITSZ2020_OS/MyEXT2/src/sh.c" "/home/huhu/HITSZ2020_OS/MyEXT2/build/CMakeFiles/main.dir/src/sh.c.o"
+  "/home/huhu/HITSZ2020_OS/MyEXT2/src/syscall.c" "/home/huhu/HITSZ2020_OS/MyEXT2/build/CMakeFiles/main.dir/src/syscall.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
